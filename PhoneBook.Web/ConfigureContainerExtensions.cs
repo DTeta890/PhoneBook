baf3add9cea93bb501a9af4931306dc8638c6c18
Deivid Teta <dteta890@gmail.com>
2022-06-18 15:05:14 +0200

    Dependency Injection

---
+++ PhoneBook.Web/ConfigureContainerExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PhoneBook.Repository;
+using PhoneBook.Repository.Repositories;
+using PhoneBook.Services.Services;
+
+namespace PhoneBook.Web
+{
+    public static class ConfigureContainerExtensions
+    {
+        public static void AddDbContext(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<DatabaseContext>(options =>
+                options.UseNpgsql(new DatabaseConfiguration().GetDataConnectionString()));
+        }
+
+        public static void AddRepositories(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<ContactRepository, ContactRepository>();
+            serviceCollection.AddScoped<ContactInformationRepository, ContactInformationRepository>();
+            serviceCollection.AddScoped<InformationTypeRepository, InformationTypeRepository>();
+        }
+
+        public static void AddTransientServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IContactService, ContactService>();
+            serviceCollection.AddTransient<IContactInformationService, ContactInformationService>();
+            serviceCollection.AddTransient<IInformationTypeService, InformationTypeService>();
+        }
+    }
+}
