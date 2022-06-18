1acc83773c7e0202058d149d66b159b49f379daf
Deivid Teta <dteta890@gmail.com>
2022-06-18 14:38:03 +0200

    Data Layer Composition, Setting up entities and relationships

---
+++ PhoneBook.Data/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PhoneBook.Data
+{
+    public class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UUID { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
